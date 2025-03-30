01a081d1d1f4edb0c7e41d0376264ab44ba26802
Guilherme Quintino <guilherme.goq@gmail.com>
2025-03-30 09:39:54 -0300

    integrate boostrap

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import 'bootstrap-icons/font/bootstrap-icons.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.min.css' // Import Bootstrap CSS
+import 'bootstrap' // Import Bootstrap JS
 
 createApp(App).mount('#app')
