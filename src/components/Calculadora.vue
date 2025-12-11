4808e894083467e148ef27004c02777371d92d88
Guilherme Quintino <guilherme.goq@gmail.com>
2025-12-11 17:00:37 -0300

    update news section

--- src/components/Calculadora.vue
+++ src/components/Calculadora.vue
@@ -58,20 +58,15 @@
         </p>
       </div>
 
-      <div class="callout callout-info">
+      <!--       <div class="callout callout-info">
         <small
           ><strong>Atualizações [01/11/2025]:</strong><br />
           📈 O cálculo da URV já está incorporado no simulador, em conformidade com a
           <a href="https://doe.to.gov.br/diario/5476/download"
             >Lei Nº 4.815, de 21 de julho de 2025</a
-          >.<br />
-          📢 Para servidores que adiantaram a 1ª parcela do 13º, o simulador compensará a diferença
-          da URV diretamente na 2ª parcela.<br />
-          ⚠️ O cálculo da previdência do IGEPREV para servidores que contribuem apenas até o teto
-          voltou a ser uma dedução simples de 14% sobre a base de cálculo, conforme a folha de
-          pagamento de out/2025.<br
-        /></small>
-      </div>
+          >.</small
+        >
+      </div> -->
 
       <div style="text-align: center; margin-top: 2em; margin-bottom: 1em">
         <button type="button" class="btn clearBtn" @click="resetCalculators">
