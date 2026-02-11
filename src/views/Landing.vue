a9cb8dd7a49af80e901b381d79a8827fb68a158a
Guilherme Quintino <guilherme.goq@gmail.com>
2026-02-10 23:01:01 -0300

    update: hero section

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,39 +1,44 @@
 <template>
-  <div class="container-sm">
-    <h1 class="title">
-      Simulador da Carreira
-      <span class="gradient-text">PCCR</span> dos Servidores do TJTO
-    </h1>
+  <div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+      <div class="col-lg-7 p-3 p-lg-5 pt-lg-3">
+        <h1 class="display-5 fw-bold text-body-emphasis lh-1 mb-3">
+          Simulador da Carreira
+          <span class="gradient-text">PCCR</span> dos Servidores do TJTO
+        </h1>
+        <p class="subtitle">
+          Simule a <strong>evolução na carreira e salário do TJTO</strong>. Visualize progressões e
+          detalhes da folha de pagamento neste simulador desenvolvido para facilitar seu
+          planejamento financeiro.
+        </p>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-start mb-4 mb-lg-3">
+          <router-link to="/app" class="btn btn-primary btn-lg px-4 gap-3"
+            ><i class="bi bi-calculator-fill me-2"></i> Simular
+          </router-link>
+          <Share />
+        </div>
+      </div>
+      <div class="col-lg-4 offset-lg-1 p-0 overflow-hidden shadow-lg">
+        <img class="rounded-lg-3" src="/hero-image.webp" alt="" width="720" />
+      </div>
+    </div>
+  </div>
 
-    <div class="d-block d-md-flex gap-3">
-      <p class="subtitle">
-        Simule a <strong>evolução na carreira e salário do TJTO</strong>. Visualize progressões e
-        detalhes da folha de pagamento neste simulador gratuito desenvolvido para facilitar seu
-        planejamento financeiro.
-      </p>
-    </div>
-
-    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <router-link to="/app" class="btn btn-primary btn-lg px-4 gap-3"
-        ><i class="bi bi-calculator-fill me-2"></i> Simular
-      </router-link>
-      <Share />
-    </div>
-    <div>
-      <main>
-        <Features />
-      </main>
-    </div>
-    <div>
-      <main>
-        <Changelog />
-      </main>
-    </div>
-    <div>
-      <main>
-        <FAQ />
-      </main>
-    </div>
+  <div class="d-grid gap-2 d-sm-flex justify-content-sm-center"></div>
+  <div>
+    <main>
+      <Features />
+    </main>
+  </div>
+  <div>
+    <main>
+      <Changelog />
+    </main>
+  </div>
+  <div>
+    <main>
+      <FAQ />
+    </main>
   </div>
 </template>
 
